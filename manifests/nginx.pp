c227d187bcad78588e599d14d1b7a7aa904d46e0
root <root@master-pp.localdomain>
2018-06-20 15:40:40 +0300

    modified:   manifests/nginx.pp

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,10 +1,18 @@
+package { 'policycoreutils':
+	ensure => installed,
+}
+exec { 'add_se_rule':
+        command 	=> '/usr/sbin/semanage port -a -t http_port_t -p tcp 8888',
+        unless   	=> '/usr/sbin/semanage port -l | /usr/bin/grep -E ^http_port_t.*8888',
+}
 class{ 'nginx' :
-    manage_repo 	     => true,
-    package_source 	     => 'nginx-mainline'
+	require => Exec['add_se_rule'],
+	manage_repo 	     => true,
+	package_source 	     => 'nginx-mainline',
 }
 nginx::resource::server { 'slave-pp.local':
         listen_port 	     => 8888,
         ensure               => present,
         server_name          => ['slave-pp.local slave-pp'],
-        proxy 	             => 'http://10.191.0.101:8080',
+	proxy 	             => "http://${facts['networking']['ip']}:8080",
 }
